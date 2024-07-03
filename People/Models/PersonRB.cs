ef24d82904d1f5808520144dd5c3eee22e9496ba
cisco <cisco@UPOAULA10310>
2024-07-03 11:11:40 -0500

    Agregar archivos de proyecto.

---
+++ People/Models/PersonRB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+namespace People.Models;
+
+[Table("people")]
+public class PersonRB
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+}
